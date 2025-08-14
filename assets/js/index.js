870db11862feb1e2e5bfbd85e4178ba2be481e0a
Abraham Pech <abrahamayil415@gmail.com>
2025-08-13 19:05:16 -0600

    Página de inicio de sesión y registros

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,13 @@
-// //Cambio de fondo cada cierto tiempo
-// const backgroundImages = [
-//     "src/img/deco1.jpg",
-//     "src/img/deco2.jpg"
-// ];
-
-// let cont = 0;
-
-// function changeBackground() {
-//   document.body.style.backgroundImage = `url("${backgroundImages[cont]}")`;
-//   cont = (cont + 1) % backgroundImages.length;
-// }
-
-// setInterval(changeBackground, 3000);
-
-// window.onload = changeBackground;
-
 const showPassword = document.querySelector(".fa-eye-slash");
 const contraseñaLogin = document.getElementById("login-pwd");
+const contraseñaVendedor = document.getElementById("contraseña-vendedor");
+const contraseñaAdmin = document.getElementById("contraseña-admin");
 const inicioSesion = document.getElementById("inicio-sesion");
 const registroAdmin = document.getElementById("registro-admin");
 const registroVendedor = document.getElementById("registro-vendedor");
+const campos = document.querySelectorAll(".inputs-general");
+
+AOS.init();
 
 //No dejar copiar los contenidos de las contraseñas
 function bloquearCopiadoContraseñas(contraseñaLogin) {
@@ -35,24 +23,63 @@ function bloquearCopiadoContraseñas(contraseñaLogin) {
   });
 }
 
-function revealPassword(){
-    const campoContraseña = contraseñaLogin.type == "password";
-    contraseñaLogin.type = campoContraseña ? "text": "password";
-    showPassword.classList.toggle("fa-eye");
-    showPassword.classList.toggle("fa-eye-slah");
+//Revelar contraseñas
+function revealPassword(icono) {
+  const contenedor = icono.parentElement;
+  const input = contenedor.querySelector("input");
 
-    if(campoContraseña){
-        bloquearCopiadoContraseñas(contraseñaLogin);
-    }
+  const esOculta = input.type === "password";
+  input.type = esOculta ? "text" : "password";
+
+  icono.classList.toggle("fa-eye");
+  icono.classList.toggle("fa-eye-slash");
+
+  if (esOculta) {
+    bloquearCopiadoContraseñas(input);
+  }
 }
 
-function revealAdminRegister(){
-    inicioSesion.style.display = "none";
-    registroAdmin.style.display = "block";
+//Funciones para mostrar los componentes
+function revealAdminRegister() {
+  inicioSesion.style.display = "none";
+  registroAdmin.style.display = "flex";
+
+  campos.forEach((input) => {
+    input.value = "";
+  });
 }
 
-function revealLogin(){
-    registroAdmin.style.display = "none";
-    registroVendedor.style.display = "none";
-    inicioSesion.style.display = "block";
+function revealLogin() {
+  registroAdmin.style.display = "none";
+  registroVendedor.style.display = "none";
+  inicioSesion.style.display = "block";
+  campos.forEach((input) => {
+    input.value = "";
+  });
 }
+
+function revealVendedorRegister() {
+  inicioSesion.style.display = "none";
+  registroVendedor.style.display = "block";
+
+  campos.forEach((input) => {
+    input.value = "";
+  });
+}
+
+// //Cambio de fondo cada cierto tiempo
+// const backgroundImages = [
+//     "src/img/fondo.webp",
+//     "src/img/fondo2.webp"
+// ];
+
+// let cont = 0;
+
+// function changeBackground() {
+//   document.body.style.backgroundImage = `url("${backgroundImages[cont]}")`;
+//   cont = (cont + 1) % backgroundImages.length;
+// }
+
+// setInterval(changeBackground, 3000);
+
+// window.onload = changeBackground;
