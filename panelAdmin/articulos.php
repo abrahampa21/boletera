f108d2b1b8af86d907599c8eee4e83030c1e5960
Romserc3423 <leon45sergio@gmail.com>
2025-08-15 22:58:33 -0600

    Registro de Articulos funcionando con DB

--- panelAdmin/articulos.php
+++ panelAdmin/articulos.php
@@ -1,3 +1,39 @@
+<?php
+
+// Conexión a la base de datos
+include("../src/conexion.php");
+
+if (isset($_POST["registrar-articulo"])) {
+    $nombreArticulo = mysqli_real_escape_string($conexion, $_POST['nombre-articulo']);
+    
+    // Se verifica si se subió la imagen
+    if (isset($_FILES['img-articulo']) && $_FILES['img-articulo']['error'] === 0) {
+        $img_tmp = $_FILES['img-articulo']['tmp_name'];
+        $imagenArticulo = addslashes(file_get_contents($img_tmp)); // listo para BLOB
+    } else {
+        echo "<script>alert('Error al subir la foto del articulo'); window.location='articulos.php';</script>";
+        exit();
+    }
+   // Insertar nuevo articulo
+    $sql_insert = "INSERT INTO articulo (nombreArticulo, imagen) VALUES ('$nombreArticulo', '$imagenArticulo')";
+
+    if ($conexion->query($sql_insert) === TRUE) {
+        echo "<script>
+            alert('Articulo registrado exitosamente');
+            window.location = 'articulos.php';
+        </script>";
+    } else{
+        echo "<script>alert('Error al registrar el articulo'); window.location='articulos.php';</script>";
+    }
+
+}
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -21,7 +57,7 @@
         </div>
       </template>
       <button id="registrar-articulo" ondblclick="hideFormArticulo()" onclick="revealFormArticulo()">Registrar artículo</button>
-      <form action="" class="registro-articulo" id="registro-articulo">
+     <form action="" method="POST" enctype="multipart/form-data" class="registro-articulo" id="registro-articulo">
         <h3>Ingrese los siguiente datos:</h3>
         <div class="div-nombre-articulo">
           <input
@@ -36,7 +72,7 @@
           <label for="img-articulo">Sube foto del artículo</label>
           <input type="file" name="img-articulo" id="" />
         </div>
-        <button type="submit" class="registrar-articulo">Enviar</button>
+        <button type="submit" class="registrar-articulo" name="registrar-articulo">Enviar</button>
       </form>
     </div>
 
