88c627f370fa8a0a0e682e08788a6acea0b10bfd
Abraham Pech <abrahamayil415@gmail.com>
2025-08-20 11:18:17 -0600

    Update listaClientes.php

--- panelAdmin/listaClientes.php
+++ panelAdmin/listaClientes.php
@@ -20,13 +20,14 @@
                     <th>Boletos</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody id="lista-clientes">
             </tbody>
         </table>
     </div>
 
     <a href="../panelAdmin.php"><i class="fa-solid fa-arrow-left"></i></a>
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script src="../assets/js/panelAdmin/listaClientes.js"></script>
     <script src="../assets/js/login.js"></script>
 </body>
 </html>
